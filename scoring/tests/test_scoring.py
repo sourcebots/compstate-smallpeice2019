7203a24769b93c242d45eb16ea38ada3fbc5bc01
Peter Law <PeterJCLaw@gmail.com>
2019-08-11 18:59:28 +0100

    Add an initial batch of tests for the scoring logic

--- scoring/tests/test_scoring.py
+++ scoring/tests/test_scoring.py
@@ -14,6 +14,168 @@ from score import Scorer, InvalidScoresheetException
 class ScorerTests(unittest.TestCase):
     longMessage = True
 
+    def construct_scorer(self):
+        return Scorer(self.teams_data, self.arena_data)
+
+    def assertScores(self, expected_scores):
+        scorer = self.construct_scorer()
+        actual_scores = scorer.calculate_scores()
+
+        self.assertEqual(expected_scores, actual_scores, "Wrong scores")
+
+    def setUp(self):
+        self.teams_data =  {
+            'ABC': {'zone': 0, 'tokens_held': 0},
+            'DEF': {'zone': 1, 'tokens_held': 0},
+            'GHI': {'zone': 2, 'tokens_held': 0},
+        }
+        self.arena_data = {
+            x: {
+              'tokens_platform': 0,
+              'tokens_ground': 0,
+            } for x in range(4)
+        }
+        self.extra_data = {'tokens_ground': 16}
+
+    # Too many tokens
+
+    def test_too_many_tokens_held(self):
+        # There are a total of 16 tokens
+
+        self.teams_data['ABC']['tokens_held'] = 9
+
+        scorer = self.construct_scorer()
+
+        with self.assertRaises(InvalidScoresheetException):
+            scorer.validate(self.extra_data)
+
+    def test_too_many_tokens_ground(self):
+        # There are a total of 16 tokens
+
+        self.arena_data[0]['tokens_ground'] = 9
+
+        scorer = self.construct_scorer()
+
+        with self.assertRaises(InvalidScoresheetException):
+            scorer.validate(self.extra_data)
+
+    def test_too_many_tokens_platform(self):
+        # There are a total of 16 tokens
+
+        self.arena_data[1]['tokens_platform'] = 9
+
+        scorer = self.construct_scorer()
+
+        with self.assertRaises(InvalidScoresheetException):
+            scorer.validate(self.extra_data)
+
+    def test_too_many_tokens_elsewhere(self):
+        # There are a total of 16 tokens
+
+        scorer = self.construct_scorer()
+
+        with self.assertRaises(InvalidScoresheetException):
+            scorer.validate({'tokens_ground': 20})
+
+    def test_too_many_tokens_mixed(self):
+        # There are a total of 16 tokens
+
+        self.teams_data['ABC']['tokens_held'] = 9
+        self.arena_data[0]['tokens_ground'] = 9
+        self.arena_data[1]['tokens_platform'] = 9
+
+        scorer = self.construct_scorer()
+
+        with self.assertRaises(InvalidScoresheetException):
+            scorer.validate({'tokens_ground': 2})
+
+    # Scoring logic
+
+    def test_no_tokens_moved(self):
+        self.assertScores({
+            'ABC': 0,
+            'DEF': 0,
+            'GHI': 0,
+        })
+
+    def test_one_robot_holding_token(self):
+        self.teams_data['ABC']['tokens_held'] = 1
+
+        self.assertScores({
+            'ABC': 1,
+            'DEF': 0,
+            'GHI': 0,
+        })
+
+    def test_one_ground_token(self):
+        self.arena_data[1]['tokens_ground'] = 1
+
+        self.assertScores({
+            'ABC': 0,
+            'DEF': 2,
+            'GHI': 0,
+        })
+
+    def test_two_ground_tokens(self):
+        self.arena_data[1]['tokens_ground'] = 2
+
+        self.assertScores({
+            'ABC': 0,
+            'DEF': 4,
+            'GHI': 0,
+        })
+
+    def test_one_platform_token(self):
+        self.arena_data[1]['tokens_platform'] = 1
+
+        self.assertScores({
+            'ABC': 0,
+            'DEF': 5,
+            'GHI': 0,
+        })
+
+    def test_two_platform_tokens(self):
+        self.arena_data[1]['tokens_platform'] = 2
+
+        self.assertScores({
+            'ABC': 0,
+            'DEF': 10,
+            'GHI': 0,
+        })
+
+    def test_one_ground_token_one_platform_token(self):
+        self.arena_data[1]['tokens_ground'] = 1
+        self.arena_data[1]['tokens_platform'] = 1
+
+        self.assertScores({
+            'ABC': 0,
+            'DEF': 7,
+            'GHI': 0,
+        })
+
+    def test_one_ground_token_one_platform_token_different_teams(self):
+        self.arena_data[1]['tokens_ground'] = 1
+        self.arena_data[0]['tokens_platform'] = 1
+
+        self.assertScores({
+            'ABC': 5,
+            'DEF': 2,
+            'GHI': 0,
+        })
+
+    def test_many_moved_tokens(self):
+        self.arena_data[0]['tokens_ground'] = 2
+        self.arena_data[0]['tokens_platform'] = 1
+
+        self.arena_data[1]['tokens_ground'] = 4
+        self.teams_data['DEF']['tokens_held'] = 2
+
+        self.assertScores({
+            'ABC': 9,
+            'DEF': 10,
+            'GHI': 0,
+        })
+
 
 if __name__ == '__main__':
     unittest.main()
